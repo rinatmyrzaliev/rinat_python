8e5298e0580c4c9f48b4a6d6db8c50349fa8919f
rinatmyrzaliev <rinat.myr@edu.312school.com>
2025-12-05 12:00:58 -0600

    second mini project

---
+++ 3.1_number_check_modulo/solution.py
@@ -0,0 +1,8 @@
+# This function checks if number is odd or even
+
+number = int(input("Give me a number: "))
+
+if number % 4 == 0:
+    print("This number is even")
+else:
+    print("This number is odd")
